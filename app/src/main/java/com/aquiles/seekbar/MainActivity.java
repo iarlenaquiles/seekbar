31279c8794611cb59423a7a9b7078ee785f11d45
Iarlen Aquiles <iarlensilva775@gmail.com>
2021-10-12 20:54:50 -0300

    initial commit

---
+++ app/src/main/java/com/aquiles/seekbar/MainActivity.java
@@ -0,0 +1,45 @@
+package com.aquiles.seekbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SeekBar seekBarEscala;
+    private TextView txtResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtResultado = findViewById(R.id.txtResultado);
+        seekBarEscala = findViewById(R.id.seekBarEscala);
+
+        seekBarEscala.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+               txtResultado.setText("Progresso: " + progress + "/" + seekBar.getMax());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void recuperar(View view) {
+        txtResultado.setText("escolhido: " +  seekBarEscala.getProgress());
+
+    }
+}
